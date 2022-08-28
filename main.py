6e7bd7223fa0fc2dc8aa164cabf21140de091e04
VLCTY <88147999+devVelocity@users.noreply.github.com>
2022-08-28 01:02:07 +0100

    on message update

--- main.py
+++ main.py
@@ -125,7 +125,7 @@ async def on_message(message):
                 # print(ctx.message.content)
                 if ctx.message.content.find(word) != -1:
                     print(ctx.message.content)
-                    if ctx.message.content.find("removebanword") != 1:
+                    if ctx.message.content.find("removebanword") != 1 or ctx.message.content.find("addbanword") != 1:
                         embed = discord.Embed(title="Warning!",description="You are not allowed to say that word in this server",color=15158332)
                         await ctx.send(ctx.message.author.mention,embed=embed)
                         await ctx.message.delete()
